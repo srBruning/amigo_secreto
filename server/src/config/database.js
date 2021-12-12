57cf807d5165623b32e04c7c24403eacf9e13edf
Diego Bruning <diego.brg92@gmail.com>
2021-12-12 14:54:55 -0300

    parameterize application database

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -3,7 +3,7 @@ require("dotenv-safe").config();
 module.exports = {
   "username": process.env.USERNMAE_DB,
   "password": process.env.PASSWORD_DB,
-  "database": process.env.PORTA,
+  "database": process.env.NAME_DB,
   "host": "127.0.0.1",
   "dialect": "mysql",
   "define":{
